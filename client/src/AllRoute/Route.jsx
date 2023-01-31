53c1dbb68f54d9caae5457cffc67b83d981075a8
unknown <saurav barnwal>
2023-01-31 16:39:15 +0530

    routing demo added

--- client/src/AllRoute/Route.jsx
+++ client/src/AllRoute/Route.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from '../pages/Page1'
+import Page2 from '../pages/Page2'
+import Page3 from '../pages/Page3'
 
-function Route() {
+function ALLRoute() {
   return (
-    <div>Route</div>
+    <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/page2" element={<Page2/>}/>
+        <Route path="/page3" element={<Page3/>}/>
+
+
+    </Routes>
   )
 }
 
-export default Route
+export default ALLRoute
--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,12 @@ import Login from './pages/Page1';
 import Page3 from './pages/Page3';
 import Register from './pages/Page2';
 import Page2 from './pages/Page2';
+import ALLRoute from './AllRoute/Route';
 
 function App() {
   return (
     <>
-    <Login/>
-   {/* <Page2/>
-    <Page3/> */}
+ <ALLRoute/>
     </>
   );
 }
